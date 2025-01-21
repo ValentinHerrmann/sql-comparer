40c7aff438180d26ca3d48832d137349c83f6bf2
Valentin Herrmann <valentn.herrmann@tum.de>
2025-01-21 13:10:24 +0100

    changed assertion message

--- tests/NormalizeQuery_test.py
+++ tests/NormalizeQuery_test.py
@@ -13,6 +13,7 @@ import Helper as He
 Ba.setDBName("bayern.db")
 
 class NormalizeQuery_test(unittest.TestCase):
+    maxDiff = None
 
     def readFile(self,path: str):
         with open(path, 'r') as file:
@@ -23,32 +24,37 @@ class NormalizeQuery_test(unittest.TestCase):
         td = Ba.getTableDict()
         q1 = He.normalizeSQLQuery(self.readFile("tests/v1/a"+nr+".sql"),td)
         q2 = He.normalizeSQLQuery(self.readFile("tests/v2/a"+nr+".sql"),td)
-        self.assertEqual(q1, q2) 
+        if q1 != q2:
+            self.fail("\n" + q1 + "\n" + q2)
     
     def test_a02_normalize_euqiv(self):
         nr = '02'
         td = Ba.getTableDict()
         q1 = He.normalizeSQLQuery(self.readFile("tests/v1/a"+nr+".sql"),td)
         q2 = He.normalizeSQLQuery(self.readFile("tests/v2/a"+nr+".sql"),td)
-        self.assertEqual(q1, q2)
+        if q1 != q2:
+            self.fail("\n" + q1 + "\n" + q2)
 
     def test_a03_normalize_euqiv(self):
         nr = '03'
         td = Ba.getTableDict()
         q1 = He.normalizeSQLQuery(self.readFile("tests/v1/a"+nr+".sql"),td)
         q2 = He.normalizeSQLQuery(self.readFile("tests/v2/a"+nr+".sql"),td)
-        self.assertEqual(q1, q2)
+        if q1 != q2:
+            self.fail("\n" + q1 + "\n" + q2)
 
     def test_a04_normalize_euqiv(self):
         nr = '04'
         td = Ba.getTableDict()
         q1 = He.normalizeSQLQuery(self.readFile("tests/v1/a"+nr+".sql"),td)
         q2 = He.normalizeSQLQuery(self.readFile("tests/v2/a"+nr+".sql"),td)
-        self.assertEqual(q1, q2)
+        if q1 != q2:
+            self.fail("\n" + q1 + "\n" + q2)
 
     def test_a05_normalize_euqiv(self):
         nr = '05'
         td = Ba.getTableDict()
         q1 = He.normalizeSQLQuery(self.readFile("tests/v1/a"+nr+".sql"),td)
         q2 = He.normalizeSQLQuery(self.readFile("tests/v2/a"+nr+".sql"),td)
-        self.assertEqual(q1, q2)
+        if q1 != q2:
+            self.fail("\n" + q1 + "\n" + q2)
