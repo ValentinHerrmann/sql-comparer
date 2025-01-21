37f32bc1689db338c2419b1cf605e7cb8ac9e1fe
Valentin Herrmann <valentn.herrmann@tum.de>
2025-01-21 12:38:46 +0100

    print path

--- tests/NormalizeQuery_test.py
+++ tests/NormalizeQuery_test.py
@@ -2,6 +2,7 @@ import os, sys
 dir_path = os.path.dirname(os.path.realpath(__file__))
 parent_dir_path = os.path.abspath(os.path.join(dir_path, os.pardir))
 sys.path.insert(0, parent_dir_path)
+print("Set path to: "+parent_dir_path)
 
 import unittest
 import BaseAccess as Ba
